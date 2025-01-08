ce611ce688280b3786742682d09306ab7d57da87
Rnveer007 <rvs523474@gmail.com>
2025-01-08 17:52:01 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import CounterApp from './counterApp'
 import TodoList from './TodoList'
 
 createRoot(document.getElementById('root')).render(
-   
-    // <CounterApp />
+
     <TodoList/>
 )
