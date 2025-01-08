48236b940137f85412f8e5c5877b3315e658b155
Rnveer007 <rvs523474@gmail.com>
2025-01-08 17:46:31 +0530

    updated

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,14 +3,34 @@ import React, { useState } from 'react';
 function TodoList() {
     const [task, setTask] = useState("");
     const [updatedTask, setUpdatedTask] = useState([]);
+    const [editingId, setEditingId] = useState(null);
 
     function addTask() {
-        if (task.trim() === "") return;
-        const obj = { id: Date.now(), key: task };
-        setUpdatedTask([...updatedTask, obj]); 
-        setTask(""); 
+        if (editingId !== null) {
+            setUpdatedTask(
+                updatedTask.map((obj) =>
+                    obj.id === editingId ? { ...obj, name: task } : obj
+                )
+            );
+            setEditingId(null)
+        }
+        else {
+            const obj = { id: Date.now(), name: task };
+            setUpdatedTask([...updatedTask, obj]);
+        }
+        setTask("");
     }
 
+    function dltTask(id) {
+        setUpdatedTask(updatedTask.filter((obj) => obj.id !== id))
+    }
+    function editTask(id) {
+        const editList = updatedTask.find((obj) => obj.id === id)
+        setTask(editList.name)
+        setEditingId(id)
+
+
+    }
     return (
         <div>
             <input
@@ -19,12 +39,14 @@ function TodoList() {
                 value={task}
                 onChange={(e) => setTask(e.target.value)}
             />
-            <button onClick={addTask}>Add Task</button>
+            <button onClick={addTask}>{editingId !== null ? "Update Task" : "Add Task"}</button>
 
             <ul>
                 {updatedTask.map((item) => (
                     <li key={item.id}>
-                        {item.key}
+                        {item.name}
+                        <button onClick={() => dltTask(item.id)}>Delete</button>
+                        <button onClick={() => editTask(item.id)}> Edit </button>
                     </li>
                 ))}
             </ul>
